07347216388c4763a3cecb6aa925f32969c733a9
gonbazsolt <gonbazsolt@gmail.com>
2018-05-10 12:36:22 +0200

    3 new file by types examples

---
+++ week01/day04/coding-hours.ts
@@ -0,0 +1,4 @@
+'use strict';
+
+console.log(17 * 5 * 6 + " hours is spent with coding in a semester by an attende");
+console.log("The coding hours are " + 5 * 6 / 52 * 100 + " % of an average week work hours");
