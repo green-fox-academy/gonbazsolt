9cd7bc7325ebc4fb7bf48b0da1f1faef0e8f2bc6
gonbazsolt <gonbazsolt@gmail.com>
2018-05-11 17:27:41 +0200

    comments were erased from code

--- week01/day05/draw-diamond.ts
+++ week01/day05/draw-diamond.ts
@@ -1,4 +1,4 @@
-export{};
+export { };
 'use strict';
 
 let lineCount: number = 7;
@@ -7,22 +7,17 @@ let lineIndexSum: number;
 let lineIndexDiff: number;
 let howManyLinesToMiddle: number;
 
-//ez a valtozo segit meghatarozni a kesobbi vizsgalatnal a megfelelo intervallum szamitasat, meghatarozasat
-//az erteke azt mutatja meg, hogy a kozepso 1 vagy 2 darab vegig csillagos sorig mennyi sor van
 howManyLinesToMiddle = Math.round(lineCount / 2) - 1;
 
 for (let iter: number = 1; iter < (lineCount + 1); iter++) {
   lineString = "";
+  
   for (let iter2: number = 1; iter2 < (lineCount + 1); iter2++) {
-    //a matrixban jelenleg mennyi a sor es oszlop indexek osszege
     lineIndexSum = iter + iter2;
-    //a matrixban jelenleg mennyi a sor es oszlop indexek abszolut erteku kulonbsege
     lineIndexDiff = Math.abs(iter - iter2);
-    //meg elegansabb lenne ha a feltetel azt vizsgalna - marmint egyszerubb szintaktikaval, mert igy is ezt teszi -
-    //hogy a kulonbseg es az osszeg benne van-e adott intervallomban
-    //olyasmire gondolok hogy if sum in range() and diff in range()
-    //rovidebb es atlathatobb is lenne a kod
-    if ( ( ( lineIndexSum >= 2 + howManyLinesToMiddle) && ( lineIndexSum <= (lineCount *2 - howManyLinesToMiddle)) ) && ( (lineIndexDiff >= 0) && ( lineIndexDiff <= (lineCount - 1 -howManyLinesToMiddle)) ) ) {
+    
+    if (((lineIndexSum >= 2 + howManyLinesToMiddle) && (lineIndexSum <= (lineCount * 2 - howManyLinesToMiddle))) 
+        && ((lineIndexDiff >= 0) && (lineIndexDiff <= (lineCount - 1 - howManyLinesToMiddle)))) {
       lineString += '*';
     } else {
       lineString += ' ';
