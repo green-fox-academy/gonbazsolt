cf24d403843a4655e03bbc09d7fef4494fc6e027
gonbazsolt <gonbazsolt@gmail.com>
2018-05-14 15:15:56 +0200

    15th exercise colors

---
+++ week02/day01/colors.ts
@@ -0,0 +1,12 @@
+'use strict';
+
+let colors = []
+
+colors = [['lime', 'forest green', 'olive', 'pale green', 'spring green'], ['orange red', 'red', 'tomato'],
+           ['orchid', 'violet', 'pink', 'hot pink']];
+
+console.log(colors[0])
+console.log(colors[1])
+console.log(colors[2])
+
+//console.log(colors[0][0], colors[0][1], colors[0][2], colors[0][3], colors[0][4]);
