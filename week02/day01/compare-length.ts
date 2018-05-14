87f0fcab043106db3f0071e3bdb0619fd2e9258e
gonbazsolt <gonbazsolt@gmail.com>
2018-05-14 13:48:33 +0200

    7th exercise compare length

---
+++ week02/day01/compare-length.ts
@@ -0,0 +1,10 @@
+'use strict';
+
+let firstList: number[] = [1, 2, 3];
+let secondList: number[] = [4, 5];
+
+if (secondList.length > firstList.length) {
+  console.log('Second List has more element then First List');
+} else {
+  console.log('First List has no less element then Second List');
+}
