db63277c195690412cb752b3bebfd17a70c76d86
gonbazsolt <gonbazsolt@gmail.com>
2018-05-24 09:21:02 +0200

    7th exercise Sharpie

---
+++ week03/day03/sharpie.ts
@@ -0,0 +1,16 @@
+'use strict';
+
+class Sharpie {
+  color: string;
+  width: number;
+  inkAmount: number = 100;
+
+  constructor(color: string, width: number) {
+    this.color = color;
+    this.width = width;
+  }
+
+  use() {
+    this.inkAmount -= 1;
+  }
+}
