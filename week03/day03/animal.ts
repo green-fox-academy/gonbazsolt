c6f5d404f0621ade2b2d32fd81f439215cef2920
gonbazsolt <gonbazsolt@gmail.com>
2018-05-24 09:13:23 +0200

    6th exercise Animal

---
+++ week03/day03/animal.ts
@@ -0,0 +1,19 @@
+'use strict';
+
+class Animal {
+  hunger: number = 50;
+  thirst: number = 50;
+
+  eat() {
+    this.hunger -= 1;
+  }
+
+  drink() {
+    this.thirst -= 1;
+  }
+
+  play() {
+    this.hunger += 1;
+    this.thirst += 11;
+  }
+}
