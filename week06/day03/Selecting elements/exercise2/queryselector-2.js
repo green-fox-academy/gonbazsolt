7e96d8a553004352acdb35596c1f3a6c05787dfb
gonbazsolt <gonbazsolt@gmail.com>
2018-06-20 12:53:07 +0200

    created: index.html queryselector-2.js Selecting elements - exercise2 (by TagName and ClassName)

---
+++ week06/day03/Selecting elements/exercise2/queryselector-2.js
@@ -0,0 +1,8 @@
+'use strict';
+
+let businessLamp = [];
+
+businessLamp.push(document.getElementsByTagName('p')[0]);
+businessLamp.push(document.getElementsByClassName('b329')[0]);
+
+console.log(businessLamp);
