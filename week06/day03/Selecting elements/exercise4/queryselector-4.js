df8d04bba373d11d2a56092652b06076f2e90d9c
gonbazsolt <gonbazsolt@gmail.com>
2018-06-20 13:15:23 +0200

    created: index.html queryselector-4.js Selecting elements - exercise4 (by querySelector)

---
+++ week06/day03/Selecting elements/exercise4/queryselector-4.js
@@ -0,0 +1,9 @@
+'use strict';
+
+let noBusiness = [];
+
+noBusiness.push(document.querySelector('div#b325'));
+noBusiness.push(document.querySelector('div.b326'));
+noBusiness.push(document.querySelector('div.b329'));
+
+noBusiness.forEach(element => {console.log(element)});
