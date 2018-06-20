78e2d1c71627b289e933d33bf947de238dc91fe2
gonbazsolt <gonbazsolt@gmail.com>
2018-06-20 13:03:30 +0200

    created: index.html queryselector-3.js Selecting elements - exercise3 (by ID and ClassName)

---
+++ week06/day03/Selecting elements/exercise3/queryselector-3.js
@@ -0,0 +1,9 @@
+'use strict';
+
+let conceitedKing = [];
+
+conceitedKing.push(document.getElementById('b325'));
+conceitedKing.push(document.getElementsByClassName('b326')[0]);
+
+alert(conceitedKing[0]);
+alert(conceitedKing[1]);
