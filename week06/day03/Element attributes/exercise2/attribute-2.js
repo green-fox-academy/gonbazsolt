ff71e45865de237d22bc2ac78088373f7a078623
gonbazsolt <gonbazsolt@gmail.com>
2018-06-20 15:02:43 +0200

    created: index.html attribute-2.js Element attributes - exercise2 (change img src)

---
+++ week06/day03/Element attributes/exercise2/attribute-2.js
@@ -0,0 +1,3 @@
+let img = document.body.querySelector('img');
+
+console.log(img.setAttribute('src', 'http://www.bumm.sk/uploads/news/102/140102/oriasi-tevedes-mergezo-gomba-evesere-buzditotta-olvasoit-egy-gyermekujsag_140102.jpg'));
