b00818fb87e3a1b1ac95018094450dc64e0c9178
gonbazsolt <gonbazsolt@gmail.com>
2018-06-20 17:26:46 +0200

    created: svg-1.html svg-1.js SVG Elements (Optional) - (change attributes)

---
+++ week06/day03/SVG Elements (Optional)/svg-1.js
@@ -0,0 +1,5 @@
+let rect = document.querySelector('rect#very_rectangle');
+
+rect.setAttribute('x', '50');
+rect.setAttribute('y', '50');
+rect.setAttribute('style', 'stroke:#ff0000; stroke-width: 10; fill: tomato');
