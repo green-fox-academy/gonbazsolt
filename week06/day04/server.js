ad13b0b1f44d8c0d2e746119be8b9255979ccc83
gonbazsolt <gonbazsolt@gmail.com>
2018-06-21 11:23:13 +0200

    sample: create server and handle static files

--- week06/day04/server.js
+++ week06/day04/server.js
@@ -2,11 +2,14 @@
 
 const express = require('express');
 const app = express();
+const path = require('path');
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello');
+  res.sendFile(path.join(__dirname, './index.html'));
 });
+
+app.use('/static', express.static('static'));
 
 app.listen(PORT, () => {
   console.log(`Yey, I'm running on port ${PORT}`);
@@ -14,4 +17,18 @@ app.listen(PORT, () => {
 
 app.get('/FinalAnswer/', (req, res) => {
   res.send('42');
+});
+
+app.get('/:greet', (req, res) => {
+  res.send(req.params.greet);
+});
+
+// handle optional parameter syntax: <message>?<object field name>=<object value>
+app.get('/greet/:message', (req, res) => {
+  console.log(req.query);
+  let name = 'Fox';
+  if (req.query.name !== undefined) {
+    name = req.query.name;
+  }
+  res.send(`${req.params.message} ${name}`);
 });
