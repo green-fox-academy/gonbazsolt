e0e5fb51078505a4922073972ca2bb83d8ff5c72
gonbazsolt <gonbazsolt@gmail.com>
2018-06-27 14:15:58 +0200

    modified: index.js exercise Bookstore endpoints - List books titles

--- week07/day03/index.js
+++ week07/day03/index.js
@@ -5,6 +5,10 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 const mysql = require('mysql');
+const path = require('path');
+
+app.use('/static', express.static('static'));
+app.set('view engine', 'ejs');
 
 let conn = mysql.createConnection ({
   host: process.env.DB_HOST,
@@ -32,7 +36,43 @@ app.get('/test', (req, res) => {
       book: rows
     });
   });
-  
+});
+
+app.get('/test2', (req, res) => {
+  let sql = `SELECT book_name FROM book_mast ORDER BY book_name;`;
+
+  conn.query(sql, function(err, rows) {
+    if (err) {
+      console.log(err);
+      res.status(500).send();
+      return;
+    }
+    
+    let book_names = [];
+    rows.forEach(element => {
+      book_names.push(element.book_name);
+    });
+
+    res.render('book_names', {
+      book_names: book_names
+    });
+  });
+});
+
+app.get('/booknames', (req, res) => {
+  let sql = `SELECT book_name FROM book_mast ORDER BY book_name;`;
+
+  conn.query(sql, function(err, rows) {
+    if (err) {
+      console.log(err);
+      res.status(500).send();
+      return;
+    }
+    
+    res.json({
+      book_names: rows
+    });
+  });
 });
 
 app.listen(PORT, () => {
