74eb9c06e26211e7a36c3bcb18ab7abed8cf0e8b
gonbazsolt <gonbazsolt@gmail.com>
2018-07-02 12:55:20 +0200

    modified: dojo_poker_test.js dojo_poker.js One Pair (pair is in only one hand)

--- week08/day01/dojo_poker.js
+++ week08/day01/dojo_poker.js
@@ -9,35 +9,71 @@ module.exports = function whoIsTheWinner(blackHand, whiteHand) {
   let winnerStatus = '';
   let isWinner = false;
 
-  blackHand.forEach(element => {
-    cards.forEach((card, index) => {
-      if (element[0] === card) {
-        blackCardIndexes.push(index);
-      }
+  function isItAPair(hand) {
+    let result = {};
+    let handCardValues = [];
+    hand.forEach(element => {
+      handCardValues.push(element[0]);
     });
-  });
-  whiteHand.forEach(element => {
-    cards.forEach((card, index) => {
-      if (element[0] === card) {
-        whiteCardIndexes.push(index);
-      }
+    if (handCardValues instanceof Array) {
+      handCardValues.forEach(function (v, i) {
+        if (!result[v]) {
+          result[v] = 1;
+        } else {
+          result[v]++;
+        }
+      });
+    }
+  
+    const valueArray = Object.keys(result).filter(key => result[key] === 2);
+    if (valueArray.length !== 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  console.log('black-pair', isItAPair(blackHand), 'white-pair', isItAPair(whiteHand));
+
+  if (isItAPair(blackHand) || isItAPair(whiteHand)) {
+    if (isItAPair(blackHand)) {
+      winnerStatus = 'Black wins';
+      isWinner = true;
+    } else {
+      winnerStatus = 'White wins';
+      isWinner = true;
+    }
+  } else {
+    blackHand.forEach(element => {
+      cards.forEach((card, index) => {
+        if (element[0] === card) {
+          blackCardIndexes.push(index);
+        }
+      });
     });
-  });
-  blackCardIndexes.sort().reverse();
-  whiteCardIndexes.sort().reverse();
-
-  console.log('black', blackCardIndexes, 'white', whiteCardIndexes);
-
-  for (let i = 0; i < 5; i++) {
-    if (!isWinner) {
-      if (blackCardIndexes[i] > whiteCardIndexes[i]) {
-        winnerStatus = 'Black wins';
-        isWinner = true;
-        i = 5;
-      } else if ( blackCardIndexes[i] < whiteCardIndexes[i]) {
-        winnerStatus = 'White wins';
-        isWinner = true;
-        i = 5;
+    whiteHand.forEach(element => {
+      cards.forEach((card, index) => {
+        if (element[0] === card) {
+          whiteCardIndexes.push(index);
+        }
+      });
+    });
+    blackCardIndexes.sort().reverse();
+    whiteCardIndexes.sort().reverse();
+  
+    console.log('black', blackCardIndexes, 'white', whiteCardIndexes);
+  
+    for (let i = 0; i < 5; i++) {
+      if (!isWinner) {
+        if (blackCardIndexes[i] > whiteCardIndexes[i]) {
+          winnerStatus = 'Black wins';
+          isWinner = true;
+          i = 5;
+        } else if ( blackCardIndexes[i] < whiteCardIndexes[i]) {
+          winnerStatus = 'White wins';
+          isWinner = true;
+          i = 5;
+        }
       }
     }
   }
--- week08/day01/dojo_poker_test.js
+++ week08/day01/dojo_poker_test.js
@@ -12,7 +12,7 @@ test('poker whoIsTheWinner: Tie', t => {
   t.end();
 });
 
-test('poker whoIsTheWinner: High Card (different 1st highest card in hands)', t => {
+test('poker whoIsTheWinner: High Card (different 1st highest cards in hands)', t => {
   const actual = whoIsTheWinner([['2', 'H'], ['9', 'H'], ['10', 'H'], ['5', 'D'], ['8', 'S']],
                                 [['2', 'H'], ['3', 'H'], ['7', 'H'], ['5', 'D'], ['8', 'S']]);
   const expected = 'Black wins';
@@ -21,10 +21,19 @@ test('poker whoIsTheWinner: High Card (different 1st highest card in hands)', t 
   t.end();
 });
 
-test('poker whoIsTheWinner: High Card (same 1st highest but different 2nd highest card in hands)', t => {
-  const actual = whoIsTheWinner([['2', 'H'], ['2', 'S'], ['10', 'H'], ['9', 'D'], ['8', 'S']],
-                                [['2', 'H'], ['3', 'H'], ['10', 'H'], ['5', 'D'], ['8', 'S']]);
+test('poker whoIsTheWinner: High Card (same 1st highest but different 2nd highest cards in hands)', t => {
+  const actual = whoIsTheWinner([['2', 'H'], ['7', 'S'], ['10', 'H'], ['9', 'D'], ['8', 'S']],
+                                [['2', 'H'], ['K', 'H'], ['10', 'H'], ['5', 'D'], ['8', 'S']]);
   const expected = 'Black wins';
+
+  t.equal(actual, expected);
+  t.end();
+});
+
+test('poker whoIsTheWinner: One Pair (pair is in only one hand)', t => {
+  const actual = whoIsTheWinner([['2', 'H'], ['5', 'S'], ['10', 'H'], ['J', 'D'], ['8', 'S']],
+                                [['2', 'H'], ['10', 'D'], ['10', 'H'], ['5', 'D'], ['8', 'S']]);
+  const expected = 'White wins';
 
   t.equal(actual, expected);
   t.end();
