e9cae2a6172fcbc3a9d87a9e8a755c8bc2cbc2bd
gonbazsolt <gonbazsolt@gmail.com>
2018-07-02 11:57:56 +0200

    modified: dojo_poker.js to pass High card case (1st highest card is same in hands)

--- week08/day01/dojo_poker.js
+++ week08/day01/dojo_poker.js
@@ -4,29 +4,41 @@ const cards = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 const colors = ['H', 'S', 'C', 'D'];
 
 module.exports = function whoIsTheWinner(blackHand, whiteHand) {
-  let blackCardIndex = 0;
-  let whiteCardIndex = 0;
+  let blackCardIndexes = [];
+  let whiteCardIndexes = [];
+  let winnerStatus = '';
+  let isWinner = false;
+
   blackHand.forEach(element => {
     cards.forEach((card, index) => {
-      if (element[0] === card && blackCardIndex < index) {
-        blackCardIndex = index;
-        console.log(card, blackCardIndex);
+      if (element[0] === card) {
+        blackCardIndexes.push(index);
       }
     });
   });
   whiteHand.forEach(element => {
     cards.forEach((card, index) => {
-      if (element[0] === card && whiteCardIndex < index) {
-        whiteCardIndex = index;
+      if (element[0] === card) {
+        whiteCardIndexes.push(index);
       }
     });
   });
-  let winnerStatus = '';
-  if (blackCardIndex > whiteCardIndex) {
-    winnerStatus = 'Black wins';
-  } else if (whiteCardIndex > blackCardIndex) {
-    winnerStatus = 'White wins';
-  } else {
+
+  for (let i = 0; i < 5; i++) {
+    if (!isWinner) {
+      if (blackCardIndexes[i] > whiteCardIndexes[i]) {
+        winnerStatus = 'Black wins';
+        isWinner = true;
+        i = 5;
+      } else if ( blackCardIndexes[i] < whiteCardIndexes[i]) {
+        winnerStatus = 'White wins';
+        isWinner = true;
+        i = 5;
+      }
+    }
+  }
+
+  if (!isWinner) {
     winnerStatus = 'It\'s a draw';
   }
   return winnerStatus;
