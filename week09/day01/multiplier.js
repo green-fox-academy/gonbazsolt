c527f662903cc64995d0e90ea36fbaf08d541158
gonbazsolt <gonbazsolt@gmail.com>
2018-07-09 16:43:48 +0200

    modified: multiplier.js exercise Multiplier (one more solution)

--- week09/day01/multiplier.js
+++ week09/day01/multiplier.js
@@ -13,3 +13,19 @@ const threeTimes = multiplier(3);
 
 console.log(threeTimes(5)); // should log 15
 console.log(threeTimes(100)); // should log 300
+
+//----------------------------------------------------------------
+
+function multiplier2(multi) {
+  return this * multi;
+}
+
+const duplicator2 = multiplier2.bind(2);
+
+console.log(duplicator2(5)); // should log 10
+console.log(duplicator2(10)); // should log 20
+
+const threeTimes2 = multiplier2.bind(3);
+
+console.log(threeTimes2(5)); // should log 15
+console.log(threeTimes2(100)); // should log 300
