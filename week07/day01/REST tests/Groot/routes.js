9e992e4a5d4c1fbce0cda1707b3babfdcada4f19
gonbazsolt <gonbazsolt@gmail.com>
2018-07-11 10:47:31 +0200

    modified: routes.js exercise Groot(comment lines deleted)

--- week07/day01/REST tests/Groot/routes.js
+++ week07/day01/REST tests/Groot/routes.js
@@ -2,10 +2,6 @@
 
 const express = require('express');
 const app = express();
-//const path = require('path');
-
-//app.use(express.json());
-//app.use('/assets', express.static('assets'));
 
 app.get('/groot', (req, res) => {
   let answer;
